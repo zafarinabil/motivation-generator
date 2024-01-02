8f9b649ffc010885330e22e2c18cd1e25b47c102
Nabil Zafari <zafarinabil@gmail.com>
2024-01-02 17:44:25 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const quoteNumberText = document.getElementById('quoteNumberText');
+const quoteText = document.getElementById('quoteText');
+const generateButton = document.getElementById('generateButton');
+
+// let quoteNumber = 0;
+
+generateButton.addEventListener('click', fetchQuote);
+
+async function fetchQuote() {
+  try {
+    // quoteNumber++;
+    // quoteNumberText.textContent = `ADVICE #${quoteNumber}`;
+    
+    const response = await fetch('https://api.adviceslip.com/advice');
+    const data = await response.json();
+    const quote = data.slip.advice;
+    quoteText.textContent = '"' + quote + '"';
+    quoteNumberText.innerHTML = `Advice # ${data.slip.id}`;
+  } catch (error) {
+    console.error('Error fetching quote:', error);
+    quoteText.textContent = "Failed to fetch quote.";
+  }
+}
+
+// Initial fetch on page load
+fetchQuote();
